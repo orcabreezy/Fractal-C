17a09843e97019916af132115d3044bdf5f773e2
eric <erxc.ceo@gmail.com>
2025-01-26 18:58:23 +0100

    moved from raw pointer to vector representation

--- include/PureImage.h
+++ include/PureImage.h
@@ -1,26 +1,38 @@
 #pragma once
 
 #include <cstddef>
-#include <memory>
+#include <vector>
 
 #include "Pixel.h"
 
 /**
- * @brief raw representation of an rgb image Bitmap
+ * @brief raw representation of a 24-bit rgb bitmap.
+ *      
+ *  the underlying datastructure is a vector^2, in `imageData` which holds the image in the following struture:
+ *     
+ *             [, [ Pixel, Pixel, Pixel, ...(height) Pixel], [], [], ...(width) [] ] 
  * 
+ *        imageData[width][height]
  */
 struct PureImage {
 
-    typedef std::unique_ptr<std::unique_ptr<Pixel[]>[]> bitmap_t;
+    typedef std::vector<std::vector<Pixel>> bitmap_t;
 
     size_t width;
     size_t height;
+    bitmap_t imageData;
 
     PureImage(size_t width, size_t height);
     PureImage(size_t width, size_t height, Pixel** imageArray);
     PureImage(const PureImage& cpy);
+
+    PureImage operator=(const PureImage& cpy);
     ~PureImage() = default;
 
-    bitmap_t imageData;
+    /**
+     * @brief unresolve the standard bayer-mosaic
+     * 
+     */
+    void standard_bayerize();
     
 };
--- src/PureImage.cpp
+++ src/PureImage.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <fstream>
-#include <cstdint>
 #include <cstring>
 
 #include "PureImage.h"
@@ -10,31 +7,97 @@
 PureImage::PureImage(size_t width, size_t height)
     : width(width), height(height) {
     
-    this->imageData = std::make_unique<std::unique_ptr<Pixel[]>[]>(this->width);
+    this->imageData = std::vector<std::vector<Pixel>>();
+    this->imageData.reserve(width);
 
     for (size_t i = 0; i < this->width; ++i) {
-        this->imageData[i] = std::make_unique<Pixel[]>(this->height);
+        std::vector<Pixel> innerVector = std::vector<Pixel>(height);
+        this->imageData.push_back(innerVector);
     }
+
 }
 
 PureImage::PureImage(size_t width, size_t height, Pixel** imageArray)
     : width(width), height(height) {
     
-    this->imageData = std::make_unique<std::unique_ptr<Pixel[]>[]>(this->width);
+    this->imageData = std::vector<std::vector<Pixel>>();
+    this->imageData.reserve(width);
 
     for (size_t i = 0; i < width; ++i) {
-        this->imageData[i] = std::make_unique<Pixel[]>(height);
-        
-        for (size_t j = 0; j < width; ++j) {
-            this->imageData[i][j] = imageArray[i][j];
+        std::vector<Pixel> innerVector = std::vector<Pixel>();
+        innerVector.reserve(height);
+        for (size_t j = 0; j < height; ++j) {
+            innerVector.emplace_back(imageArray[i][j]);
         }
+        this->imageData.push_back(innerVector);
     }
 }
 
 PureImage::PureImage(const PureImage& cpy)
     : PureImage(cpy.width, cpy.height) {
 
-    for (size_t i = 0; i < this->width; ++i) {
-        std::memcpy(imageData[i].get(), cpy.imageData[i].get(), this->height * sizeof(Pixel));
+    this->imageData = cpy.imageData;
+}
+
+PureImage PureImage::operator=(const PureImage& cpy) {
+    return PureImage(cpy);
+}
+
+int determineColor(int i, int j) {
+
+    int r = 0;
+    int g = 1;
+    int b = 2;
+
+    if (i % 2 == 0) {
+        // g-b row
+
+        if (j % 2 == 0) {
+            return g;
+        }
+
+        return b;
+    }
+    
+    // r-g row
+    if (j % 2 == 0) {
+
+        return r;
+    }
+    return g;
+}
+
+void PureImage::standard_bayerize() {
+
+    PureImage& newImage = *this;
+
+    int r = 0;
+    int g = 1;
+    int b = 2;   
+
+    for (size_t i = 0; i < newImage.width; i++) {
+
+        for (size_t j = 0; j < newImage.height; j++) {
+            
+            int color = determineColor(i, j);             
+
+            if (color == r) {
+
+                newImage.imageData[i][j].b = 0;
+                newImage.imageData[i][j].g = 0;
+            }
+
+            else if (color == g) {
+
+                newImage.imageData[i][j].r = 0;
+                newImage.imageData[i][j].b = 0;
+            }
+
+            else {
+
+                newImage.imageData[i][j].r = 0;
+                newImage.imageData[i][j].g = 0;
+            }
+        }
     }
 }
