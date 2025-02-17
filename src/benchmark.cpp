58f2ce886a74bdda6b4dae12866421ce0697026b
eric <erxc.ceo@gmail.com>
2025-02-17 19:29:24 +0100

    added useful benchmarking functions

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,16 +1,58 @@
-#pragma once
-
 #include <chrono>
+#include <numeric>
 
-double benchmark(void (*f)(const size_t), const size_t size) {
+#include "fractal/Mandelbrot.h"
+#include "fractal/FractalImage.h"
+#include "BmpImage.h"
+
+double benchmark(void (*f)(const size_t), const size_t problem_size) {
 
     auto start = std::chrono::high_resolution_clock::now();
-    f(size);
+    f(problem_size);
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> duration = end - start;
     return duration.count();
 }
+
+std::vector<double> benchmarks(void (*f)(const size_t), const size_t problem_size, const size_t n) {
+
+    std::vector<double> durations;
+    durations.reserve(n);
+
+    for (int i = 0; i < n; i++) {
+        durations.push_back(benchmark(f, problem_size));
+    }
+
+    return durations;
+}
+
+double arithmeticMean(std::vector<double> values) {
+
+    return std::accumulate(
+        std::begin(values),
+        std::end(values),
+        0.0,
+        [](double a, double b){return a + b;}
+    ) / values.size();
+}
+
+std::string statisticalBenchmark(void (*f)(const size_t), const size_t problem_size, const size_t n) {
+
+    std::vector<double> durations(benchmarks(f, problem_size, n));
+
+    double mean = arithmeticMean(durations);
+
+    std::stringstream summary;
+    for (double duration : durations) {
+        summary << duration << "\n";
+    }
+
+    summary << "\n-------\n" << "mean: " << mean << std::endl;
+
+    return summary.str();
+}
+
 
 void true_matrix(const size_t size) {
 
@@ -65,5 +107,13 @@ void flat_matrix(const size_t size) {
         matrix[i] = matrix[i] * matrix[i];
     }
     delete[] matrix;
+}
+
+void mandelbrot_rendering(const size_t size) {
+
+  Mandelbrot fr;
+  FractalImage fi(1920, 1080, &fr);
+  PureImage pi(fi.getGradualImage());
+  BmpImage imageFile(pi);
 }
 
